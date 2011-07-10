326f621e2c1a50e11ad1a75f992774bfd3085e9d
Andrew Brown <andrew@casabrown.com>
2011-07-10 12:47:39 -0400

    Rewrite of 'Template' 	Added usage comments 	Added type constants 	Added specific functions for each type of 'Templating' 	Bug Fixes

--- Basic.Template.php
+++ Basic.Template.php
@@ -2,30 +2,89 @@
 /**
  * @copyright Copyright 2009 Gearbox Studios. All rights reserved.
  * @license GNU/GPL, see 'help/LICENSE.html'.
+ * 
+ * Should work like:
+ *  $t = new Template('file.html', Template::FILE);
+ *  $t->replace('var', 'some value...');
+ *  $t->display();
  */
 
+// TODO: split replace into replace, replaceFromPHP, replaceFromFile
+// TODO: split out __construct as well
 class Template{
 
     /**
      * Working text
      * @var <string>
      */
-    public $text;
+    private $text;
+    
+    /**
+     * Attempts to use TidyHTML to clean up code
+     * @var bool 
+     */
+    private $tidy = true;
+    
+    /**
+     * Inserts the file name of a template as a comment before the template begins
+     * E.g.: <!-- template.html -->
+     * @var bool 
+     */
+    private $insert_comment = true;
 
+    /**
+     * Types of input
+     */
+    const STRING = 0;
+    const FILE = 1;
+    const PHP_STRING = 2;
+    const PHP_FILE = 3;
+    
     /**
      * Constructor
      * @param <string> Path/Text
      */
-    public function __construct($string = null, $is_path = false){
-        if( !$string ) return;
-        if( $this->isPath($string) || $is_path ){
-            $this->text = $this->getTextFromPath($string);
+    public function __construct($input, $type = self::FILE){
+        // turn off insert_comment
+        $on_off = $this->insert_comment;
+        $this->setInsertComment(false);
+        // get first text
+        switch($type){
+            case self::STRING: $this->text = $input; break;
+            case self::FILE: $this->text = $this->getFile($input); break;
+            case self::PHP_STRING: $this->text = $this->getPHPString($input); break;
+            case self::PHP_FILE: $this->text = $this->getPHPFile($input); break;
         }
-        else{
-            $this->text = $this->getTextFromText($string);
-        }
+        // reset insert_comment
+        $this->setInsertComment($on_off);
     }
 
+    /**
+     * Sets text
+     * @param string $text 
+     */
+    public function setText($text){
+        // set text
+        $this->text = $text;
+    }
+    
+    /**
+     * Sets TidyHTML option; if true, will attempt to fix and format HTML
+     * @param bool $on_off 
+     */
+    public function setTidy($on_off){
+        $this->tidy = $on_off;
+    }
+    
+    /**
+     * Sets insert_commment; if true, the file/token name of a template will be 
+     * printed as a comment before the template code
+     * @param type $on_off 
+     */
+    public function setInsertComment($on_off){
+        $this->insert_file_comment = $on_off;
+    }
+    
     /**
      * Checks whether given string is a likely path
      * @param <string> $path
@@ -44,51 +103,9 @@ class Template{
     }
 
     /**
-     * Gets text from given string (does not evaluate PHP code by default)
-     * @param <string> $text
-     * @param <boolean> $evaluate text as code
-     * @param <array> $variables for evaluation context
-     */
-    private function getTextFromText( $text, $evaluate = false, $variables = array() ){
-        if( $evaluate ){
-            // in case of extracted variables overwrite
-            $_unlikely_variable_name = $text;
-            // extract context variables
-            if( is_array($variables) ) extract($variables);
-            // eval code
-            ob_start();
-            eval('?>'.$_unlikely_variable_name);
-            return ob_get_clean();
-        }
-        else{
-            return $text;
-        }
-    }
-
-    /**
-     * Sets template text from a given path (evaluates PHP code by default)
-     * @param <string> $text
-     * @param <boolean> $evaluate text as code
-     * @param <array> $variables for evaluation context
-     */
-    private function getTextFromPath ($path, $evaluate = true, $variables = array() ){
-        $path = $this->getAbsolutePath($path);
-        // get contents or set text as error
-        if( is_file($path) ){
-            $text = file_get_contents($path);
-        }
-        else{
-            $text = '<b>Error:</b> Could not find '.$path;
-        }
-        // reuse FromText to evaluate PHP code
-        return $this->getTextFromText($text, $evaluate, $variables);
-    }
-
-    /**
      * Get valid path
      * @param <string> $path
      * @return <string>
-     */
     private function getAbsolutePath($path){
         $config = Configuration::getInstance();
         if( array_key_exists('template_path', $config) ) $path = $config['template_path'].$path;
@@ -96,26 +113,114 @@ class Template{
         $path = str_replace('//', '/', $path);
         return $path;
     }
+     * 
+     */
 
     /**
-     * Replace
-     * @param <string> $token to replace
-     * @param <string> $string to replace with
-     * @param <boolean> $evaluate string as code
-     * @param <array> $context_variables to use in evaluated template code
+     * Replace token with string
+     * @param string $token to replace
+     * @param string $string to replace with
      */
-    public function replace($token, $string, $evaluate = 'default', $variables = array() ){
-        if( $string && $this->isPath($string) ){
-            if( $evaluate == 'default' ) $evaluate = true;
-            $text = $this->getTextFromPath($string, $evaluate, $variables);
+    public function replace($token, $string ){
+        $token = '<K:'.$token.'/>';
+        // replace
+        $this->text = str_ireplace($token, $string, $this->text);
+    }
+   
+    /**
+     * Returns file as string; attempts to find file using base_dir
+     * @param string $file
+     * @return type 
+     */
+    public function getFile($file){
+        // check file
+        if( !is_file($file) ){
+            $file = Configuration::get('base_dir').DS.$file;
+            if( !is_file($file) ) return '<b>Error:</b> Could not find '.$file;;
         }
-        else{
-            if( $evaluate == 'default' ) $evaluate = false;
-            $text = $this->getTextFromText($string, $evaluate, $variables);
+        // return
+        return file_get_contents($file);
+    }
+    
+    /**
+     * Replaces token with simple file output
+     * @param string $token
+     * @param string $file
+     */
+    public function replaceFromFile($token, $file){
+        $string = $this->getFile($file);
+        // insert comment
+        if( $this->insert_comment ){
+            $_file = basename($file);
+            $string = "<!-- FILE = $_file, TOKEN = $token -->\n".$string;
         }
         // replace
-        $token = '<K:'.$token.'/>';
-        $this->text = str_ireplace($token, $text, $this->text);
+        $this->replace($token, $this->getFile($file));
+    }
+    
+    /**
+     * Returns evaluated string from PHP code
+     * @param type $string__hide__
+     * @param type $variables__hide__
+     * @return type 
+     */
+    public function getPHPString($string__hide__, $variables__hide__ = array()){
+        // extract context variables
+        if( is_array($variables__hide__) ) extract($variables__hide__);
+        // eval code
+        ob_start();
+        eval('?>'.$string__hide__);
+        return ob_get_clean();
+    }
+    
+    /**
+     * Replaces token with an evaluated PHP string (strange var names are to avoid collisions with extract)
+     * @param string $token__hide__
+     * @param string $string__hide__
+     * @param array $variables__hide__ 
+     */
+    public function replaceFromPHPString($token, $string, $variables = array()){
+        $string = $this->getPHPString($string, $variables);
+        // insert comment
+        if( $this->insert_comment ) $string = "<!-- PHP STRING, TOKEN = $token -->\n".$string;
+        // replace
+        $this->replace($token, $string);
+    }
+    
+    /**
+     * Returns evaluated string of PHP file (strange var names are to avoid collisions with extract)
+     * @param type $file__hide__
+     * @param type $variables__hide__
+     * @return type 
+     */
+    public function getPHPFile($file__hide__, $variables__hide__ = array()){
+        // check file
+        if( !is_file($file__hide__) ){
+            $file__hide__ = Configuration::get('base_dir').DS.$file__hide__;
+            if( !is_file($file__hide__) ) return '<b>Error:</b> Could not find '.$file__hide__;
+        }
+        // get output
+        if( is_array($variables__hide__) ) extract($variables__hide__);
+        ob_start();
+        require($file__hide__);
+        return ob_get_clean();
+    }
+    
+    /**
+     * Replaces token with an evaluated PHP file (strange var names are to avoid collisions with extract)
+     * @param string $token
+     * @param string $file
+     * @param array $variables 
+     */
+    public function replaceFromPHPFile($token, $file, $variables = array()){
+        $string = $this->getPHPFile($file, $variables);
+        // insert comment
+        if( $this->insert_comment ){
+            $_file = basename($file);
+            $string = "<!-- FILE = $_file, TOKEN = $token -->\n".$string;
+        }
+        // replace
+        $this->replace($token, $string);
     }
 
     /**
@@ -132,6 +237,11 @@ class Template{
      * @return <string>
      */
     public function toString(){
+        if( $this->tidy && class_exists('tidy') ){
+            $config = array('indent' => true, 'output-xhtml' => true, 'wrap' => 120, 'indent-spaces' => 4);
+            $tidy = new tidy();
+            $this->text = $tidy->repairString($this->text, $config, 'utf8');
+        }
         return $this->replaceDanglingTokens();
     }
 
