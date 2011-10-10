6d59bce82a47ac26b2308725ac23c5677044bfc5
Andrew Brown <andrew@casabrown.com>
2011-10-10 13:29:06 -0400

    Added Exception folder and new Exception types

---
+++ Exception/Web.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @copyright Copyright 2011 Andrew Brown. All rights reserved.
+ * @license GNU/GPL, see 'help/LICENSE.html'.
+ */
+
+class ExceptionWeb extends Exception{
+    
+    public function __toString(){
+        header( 'HTTP/1.1 ' . intval($this->getCode()) );
+        $out = "<h2>Web Exception ({$this->getCode()})</h2>\n";
+        $out .= "<p>{$this->getMessage()}</p>\n";
+        $out .= "<pre>{$this->getTraceAsString()}</pre>\n";
+        return $out;
+    }
+    
+}
