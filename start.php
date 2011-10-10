9364bc624d41af7f574aa630eb77a685a99ba381
Andrew Brown <andrew@casabrown.com>
2011-10-10 13:24:43 -0400

    Major refactoring, changed autoload(), moved in functions from Basic.Function

--- start.php
+++ start.php
@@ -1,14 +1,69 @@
 <?php
 /**
- * @copyright Copyright 2009 Gearbox Studios. All rights reserved.
+ * @copyright Copyright 2011 Andrew Brown. All rights reserved.
  * @license GNU/GPL, see 'help/LICENSE.html'.
  */
 
 /**
- * Include Common Functions
+ * Finds the base directory of the pocket knife installation
+ * @return <type> 
  */
-require('Basic.Functions.php');
-require('Basic.Configuration.php');
+function get_base_dir() {
+    return dirname(__FILE__);
+}
+
+/**
+ * Autoloads classes using camel-case
+ * @example Class 'ExampleClass' should be found in /base/directory/Class/Example.php
+ * @param string $class 
+ */
+function autoload($class) {
+    $replaced = preg_replace('/([a-z])([A-Z])/', '$1/$2', $class);
+    $replaced = str_replace('/', DS, $replaced);
+    $path = get_base_dir() . DS . $replaced . '.php';
+    if (!is_file($path)) {
+        throw new Exception('Class '.$class.' not found at: '.$path, 500);
+        return false;
+    }
+    require $path;
+    return true;
+}
+
+/**
+ * Hooks autoload function into PHP __autoload; will not work in PHP CLI mode
+ */
+if( !function_exists('__autoload') ){
+    function __autoload( $class ) {
+        return autoload($class);
+    }
+}
+
+/**
+ * Prints formatted contents of a variable (cakePHP-style)
+ * @param <mixed> $thing
+ * @return <boolean> true
+ */
+function pr($thing) {
+    echo '<pre>';
+    if (is_null($thing))
+        echo 'NULL';
+    elseif (is_bool($thing))
+        echo $thing ? 'TRUE' : 'FALSE';
+    else
+        print_r($thing);
+    echo '</pre>'."\n";
+    return ($thing) ? true : false; // for testing purposes
+}
+
+/**
+ * Get publicly accessible properties of an object
+ * Works by calling get_object_vars from outside the class scope
+ * @param <object> $object
+ * @return <array>
+ */
+function get_public_vars($object) {
+    return get_object_vars($object);
+}
 
 /**
  * Report errors
@@ -21,63 +76,4 @@ ERROR_REPORTING(E_ALL);
  */
 if( !defined('DS') ){
     define( 'DS', DIRECTORY_SEPARATOR );
-}
-
-/**
- * Auto-load classes
- * This function looks first throught the Pocket Knife classes, then searches
- * through the user-defined classes designated in the configuration file in the
- * 'includes' array ($config['includes']); this function will fix trailing
- * slashes. For user-defined classes, the file name should be the class name
- * with the '.php' filename extension.
- * @example Class 'ExampleClass' should be in a file 'ExampleClass.php'
- * @param <string> $class
- */
-if( !function_exists('__autoload') ){
-    function __autoload( $class ) {
-        $file = null;
-        // look in pocket-knife directory first
-        if( strpos($class, 'App') !== false ){
-            $file = preg_replace('/([a-z])([A-Z])/', '$1.$2', $class).'.php';
-        }
-        elseif( strpos($class, 'Auth') !== false ){
-            $file = 'Security.Authentication.php';
-        }
-        else{
-            // TODO: fix this mess
-            $basic = array('Cache', 'Configuration', 'Functions', 'Http', 'Inflection', 'Routing', 'Scheduler', 'Set', 'Template', 'Test', 'Timer', 'Validation', 'Session', 'Process');
-            if( in_array($class, $basic) ) $file = 'Basic.'.$class.'.php';
-        }
-        if( $file ){
-            require get_base_dir().DS.$file;
-            return true;
-        }
-        // then look in configured directories
-        $config = Configuration::getInstance();
-        if( array_key_exists('includes', $config) ){
-            if( !is_array($config['includes']) ) $config['includes'] = array($config['includes']); // TODO: fix this hack
-            foreach($config['includes'] as $dir){
-                // create path
-                $last = $dir[ strlen($dir) - 1 ];
-                $dir = $last == DS ? $dir : $dir.DS;
-                $file = $dir.$class.'.php';
-                // include
-                if( is_file($file) ) {
-                    require($file);
-                    return true;
-                }
-                // include relative paths within pocket-knife
-                elseif( $file[0] != '/' && is_file(get_base_dir().DS.$file) ){
-                    require(get_base_dir().DS.$file);
-                    return true;
-                }
-            }
-        }
-        // could not find file
-        trigger_error('Failed to find class: '.$class, E_USER_WARNING);
-        return false;
-    }
-}
-else{
-    throw_error('__autoload is not available', E_USER_WARNING);
 }
