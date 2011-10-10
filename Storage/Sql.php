d2ef13a91bee78c472b3d09c5375f6cc014444c6
Andrew Brown <andrew@casabrown.com>
2011-10-10 13:29:23 -0400

    Added Storage stubs

---
+++ Storage/Sql.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @copyright Copyright 2011 Andrew Brown. All rights reserved.
+ * @license GNU/GPL, see 'help/LICENSE.html'.
+ */
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
