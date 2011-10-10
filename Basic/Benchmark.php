85f5c3cdf37b968ff44fe603181678de7d1bcfe2
Andrew Brown <andrew@casabrown.com>
2011-10-10 13:27:14 -0400

    TODO: Benchmark stub

---
+++ Basic/Benchmark.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @copyright Copyright 2011 Andrew Brown. All rights reserved.
+ * @license GNU/GPL, see 'help/LICENSE.html'.
+ */
+
+/**
+ * BasicBenchmark
+ * @uses
+ */
+class BasicBenchmark{
+    public static function getMemory(){
+        
+    }
+    public static function startTimer(){
+        
+    }
+    public static function endTimer(){
+        
+    }
+}
