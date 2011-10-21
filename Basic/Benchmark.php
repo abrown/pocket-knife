14b43ce68d8b2212d129240b0ef9a720d0ec599f
andrew <andrew@casabrown.com>
2011-10-21 19:18:09 -0400

    New benchmarking

--- Basic/Benchmark.php
+++ Basic/Benchmark.php
@@ -10,13 +10,50 @@
  * @uses
  */
 class BasicBenchmark{
-    public static function getMemory(){
-        
-    }
+    
+    /**
+     * Timer storage
+     * @var int 
+     */
+    static $start = 0;
+    static $end = 0;
+    
+    /**
+     * Start timer
+     * @return <int>
+     */
     public static function startTimer(){
-        
+        self::$start = microtime(true);
+        return self::$start;
     }
+    
+    /**
+     * End timer
+     * @return <int>
+     */
     public static function endTimer(){
-        
+        self::$end = microtime(true);
+        return self::$end;
+    }
+    
+    /**
+     * Returns time elapsed
+     * @return <int>
+     */
+    public static function getTime(){
+        return self::$end - self::$start;
+    }
+    
+    /**
+     * Returns current memory usage
+     * @return string
+     */
+    public static function getMemory(){
+        $size = memory_get_usage(true);
+        $units = array('b','kb','mb','gb','tb','pb');
+        $i = floor( log($size,1024) );
+        $mem = $size / pow(1024, $i);
+        $mem = @round( $m, 2 );
+        return $mem.' '.$units[$i];
     }
 }
