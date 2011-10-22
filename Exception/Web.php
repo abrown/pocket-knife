9b138ee8889a23e0306741f9488b92ed4b536b44
Andrew Brown <andrew@casabrown.com>
2011-10-22 19:52:23 -0400

    Removed some newlines

--- Exception/Web.php
+++ Exception/Web.php
@@ -5,14 +5,15 @@
  * @license GNU/GPL, see 'help/LICENSE.html'.
  */
 
+/**
+ * Exception
+ */
 class ExceptionWeb extends Exception{
-    
     public function __toString(){
         header( 'HTTP/1.1 ' . intval($this->getCode()) );
         $out = "<h2>Web Exception ({$this->getCode()})</h2>\n";
         $out .= "<p>{$this->getMessage()}</p>\n";
         $out .= "<pre>{$this->getTraceAsString()}</pre>\n";
         return $out;
-    }
-    
+    }  
 }
