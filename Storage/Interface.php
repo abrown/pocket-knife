2d875f415c4df39230ff332273de3970f9635898
Andrew Brown <andrew@casabrown.com>
2011-10-26 21:21:23 -0300

    Added CRUD, search, all, last functions

--- Storage/Interface.php
+++ Storage/Interface.php
@@ -10,11 +10,16 @@
  * All Storage classes must implement these functions
  */
 interface StorageInterface{
-    /**
+
     public function begin();
     public function commit();
     public function rollback();
     public function isChanged();
-     * 
-     */
+    public function create($record, $id);
+    public function read($id);
+    public function update($record, $id);
+    public function delete($id);
+    public function search($key, $value);
+    public function all();
+    public function last();
 }
