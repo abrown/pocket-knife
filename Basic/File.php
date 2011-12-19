0bbd321d8252a66b70a98cd4da78cdeee78ae38d
Andrew Brown <andrew@casabrown.com>
2011-12-18 22:12:26 -0500

    Fixed bugs in autoloadAll TODO: move this code to BasicClass, add documentation there

--- Basic/File.php
+++ Basic/File.php
@@ -11,35 +11,67 @@
  */
 class BasicFile{
     
+    static private $autoload = array();
+    
     /**
      * Recursively loads classes
-     * @param type $class
-     * @return type 
+     * @param string $class
+     * @return boolean 
      */
     public static function autoloadAll($class){
         // check
+        if( class_exists($class, false) ) return true;
+        // get dependencies
+        $dependencies = self::findDependencies($class);
+        // get interfaces first
+        $interfaces = preg_grep('/interface/i', $dependencies);
+        foreach($interfaces as $_class){
+            if( !interface_exists($_class) ) autoload($_class);
+        }
+        // then rest of classes
+        foreach($dependencies as $_class){
+            if( !class_exists($_class) && !interface_exists($_class) ) autoload($_class);
+        }      
+        // return
+        return true;
+    }
+    
+    /**
+     * Performs breadth-first search of dependencies
+     * @param string $class
+     * @param boolean $debug
+     * @return array 
+     */
+    public function findDependencies( $class, $dependencies = array() ){
         if( !$class ) return array();
-        if( class_exists($class, false) ) return array();
-        // setup
-        $autoloaded = array();
-        // autoload class
-        autoload($class);
-        $autoloaded[] = $class;
-        // get file name so we can look at '@uses'
-        $replaced = preg_replace('/([a-z])([A-Z])/', '$1/$2', $class);
-        $replaced = str_replace('/', DS, $replaced);
-        $path = get_base_dir() . DS . $replaced . '.php';
-        if (!is_file($path)) throw new ExceptionFile('Class '.$class.' not found at: '.$path, 500);
+        // add self to dependencies
+        if( !in_array($class, $dependencies) ) $dependencies[] = $class;
+        // get path to class
+        $path = self::getPathToClass($class);
         // get dependencies from '@uses' annotation
         $content = file_get_contents($path);
         if( preg_match('/@uses (.*)/', $content, $matches) ){
             $classes = explode(',', $matches[1]);
-            foreach($classes as $class){
-                $class = trim($class);
-                $dependencies = BasicFile::autoloadAll($class);
-                $autoloaded = array_merge($autoloaded, $dependencies);
+            foreach($classes as $_class){
+                $_class = trim($_class);
+                if( in_array($_class, $dependencies) ) continue;
+                else $dependencies[] = $_class;
+                $dependencies = BasicFile::findDependencies($_class, $dependencies);
             }
         }
-        return $autoloaded;
+        return $dependencies;
+    }
+    
+    /**
+     * Returns absolute path to the class file
+     * @param string $class
+     * @return string 
+     */
+    public function getPathToClass($class){
+        $replaced = preg_replace('/([a-z])([A-Z])/', '$1/$2', $class);
+        $replaced = str_replace('/', DS, $replaced);
+        $path = get_base_dir() . DS . $replaced . '.php';
+        if (!is_file($path)) throw new ExceptionFile('Class '.$class.' not found at: '.$path, 500);
+        return $path;
     }
 }
