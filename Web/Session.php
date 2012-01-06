e138d08e468f61556ff75e1a28de685ce764b037
andrew <andrew@casabrown.com>
2012-01-06 16:15:20 -0500

    Added documentation

--- Web/Session.php
+++ Web/Session.php
@@ -4,21 +4,37 @@
  * @license GNU/GPL, see 'help/LICENSE.html'.
  */
 
-/**
- * Start tracking sessions
- */
 session_start();
 
 /**
- * WebSession
- * @uses
+ * Provides static methods to help with web sessions.
+ * @example
+ * // first request
+ * WebSession::put('var', 5);
+ * 
+ * // second request
+ * $var = WebSession::get('var');
  */
 class WebSession {
-    function get($key){
+	
+	/**
+	 * Returns a session key. This method is a wrapper for the PHP
+	 * $_SESSION array.
+	 * @param mixed $key
+	 * @return mixed
+	 */
+    static public function get($key){
         if( array_key_exists($key, $_SESSION) ) return $_SESSION[$key];
         else return null;
     }
-    function put($key, $value){
+    
+    /**
+     * Saves a session key. This method is a wrapper for the PHP
+     * $_SESSION array.
+     * @param string $key
+     * @param mixed $value
+     */
+    static public function put($key, $value){
         $_SESSION[$key] = $value;
     }
 }
