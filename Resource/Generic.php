38b7d7d0aedc9b70baf9e3100b4efe7cfc3eb851
andrew <andrew@casabrown.com>
2012-02-06 21:13:59 -0500

    ResourceStatic now is ResourceGeneric

---
+++ Resource/Generic.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * @copyright Copyright 2011 Andrew Brown. All rights reserved.
+ * @license GNU/GPL, see 'help/LICENSE.html'.
+ */
+
+/** 
+ * Provides a generic template for an object that is neither a 
+ * list nor an item in a list, but must be accessed by 
+ * a web service.
+ */
+class ResourceGeneric implements ResourceInterface{
+    
+    /**
+     * Constructor
+     */
+    /**
+    public function __construct(){
+        $properties = get_public_vars($this);
+        foreach($properties as $property => $value){
+            if( array_key_exists($property, $_GET) ){
+                $this->$property = $_GET[$property];
+            }
+        }
+    } 
+    */
+    
+    /**
+     * Returns the object URI
+     * @return string
+     */
+    public function getURI(){
+        return '/'.strtolower(get_class($this));
+    }
+       
+    /**
+     * Initializes the storage object; uses class variables to store data
+     * @return this
+     */
+    public function setStorage($settings){
+        return $this;
+    }
+    
+    /**
+     * Returns the storage object; uses class variables to store data
+     * @return this
+     */
+    public function getStorage(){
+        return $this;
+    }
+}
