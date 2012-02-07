b0729098d54b1d9a726c532960a5c5dece134e34
andrew <andrew@casabrown.com>
2012-02-06 21:16:46 -0500

    Initial testing for resources; ResourceGeneric is complete; missing List and Item

---
+++ test/ResourceTest.php
@@ -0,0 +1,114 @@
+<?php
+
+/**
+ * @copyright Copyright 2011 Andrew Brown. All rights reserved.
+ * @license GNU/GPL, see 'help/LICENSE.html'.
+ */
+
+/**
+* Replaces file_get_contents for classes that use it to get
+* request bodies; set static variable in ServiceTest::$REQUEST_BODY
+* to simulate the request
+*/
+namespace Testing;
+function file_get_contents($file){
+    if( $file == 'php://input' ) return ServiceTest::$REQUEST_BODY;
+    else return \file_get_contents($file);
+}
+
+class ResourceTest extends PHPUnit_Framework_TestCase{
+
+    public static $REQUEST_BODY = "";
+    
+    public static function setUpBeforeClass(){
+        // start pocket knife
+        $path = dirname(dirname(__FILE__));
+        require $path.'/start.php';
+        // get code
+        autoload('ResourceInterface');
+        autoload('ResourceGeneric');
+        autoload('ResourceItem');
+        autoload('ResourceList');
+    }
+
+    public function setUp(){
+        $this->expected = new stdClass();
+        $this->expected->a = 1;
+        $this->expected->b = array(2=>'some text');
+        $this->expected->b[3]->c = false;
+        $this->expected->b[3]->d = true;
+    }
+    
+    /**
+     * See example class below, ResourceGenericExample
+     */
+    public function testGenericJson(){
+        // mimic a json request
+        self::$REQUEST_BODY = "http://www.google.com";
+        $expected = "Content-Type: application/json\n\n"+
+            '{"url":"ed646a3334","time:"'.date('r').'"}';
+        $service = new Service();
+        $service->class = 'UrlShortener';
+        $service->method = 'shorten';
+        $service->content_type = 'application/json';
+        $actual = $service->execute(true);
+        $this->assertEquals($expected, $actual);
+    }
+    
+    public function testGenericHtml(){
+        // mimic an html request
+        self::$REQUEST_BODY = "http://www.google.com";
+        $expected = "Content-Type: text/html\n\n"+
+                    'The URL is: ed646a3334<br/>\n'.date('r');
+        $service = new Service();
+        $service->class = 'UrlShortener';
+        $service->method = 'shorten';
+        $service->content_type = 'text/html';
+        $actual = $service->execute(true);
+        $this->assertEquals($expected, $actual);
+    }
+}
+
+/**
+ * This class demonstrates a sample web service for 
+ * shortening URLs; no claims are made about the efficacy of
+ * the shortening algorithm. By using a generic resource, we
+ * do not have to worry about storing data; anything that needs
+ * storing will be stored in the class instance as a property.
+ */
+class UrlShortener extends ResourceGeneric{
+    public function shorten($url){
+        return substr(md5($url), 0, 10); // real classy...
+    }
+    public function fromRepresentation($content_type){
+        $url = parent::fromRepresentation($content_type);
+        return WebUrl::normalize($url);
+    }
+    public function toRepresentation($content_type, $data){
+        $representation = parent::toRepresentation($content_type, $data);
+        if( $content_type == 'text/html' ){
+            // special HTML templating
+            $representation->setTemplate(new WebTemplate("The URL is: <template:url/><br/><?php echo date('r');?>"), WebTemplate::PHP_STRING);
+            $representation->getTemplate()->replace('url', $data);
+        }
+        elseif( $content_type == 'application/octet-stream' ){
+            // can download a file by this name
+            $representation->setName("url.txt");
+        }
+        elseif( $content_type == 'application/json' ){
+            // special json format
+            $o = new stdClass;
+            $o->url = $representation->getData();
+            $o->time = date('r');
+            $representation->setData($o);
+        }
+        return $representation;
+    }
+}
+
+/**
+ * This class demonstrates a web service for a library
+ */
+class Library extends ResourceList{
+    // TODO
+}
