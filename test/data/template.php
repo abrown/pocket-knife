e5a520f1d3872940b3958b76fca784134035dd46
Andrew Brown <andrew@casabrown.com>
2012-02-17 15:42:26 -0500

    Remove old and unnecessary tests

--- test/data/template.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title><template:title/></title>
-    </head>
-    <body>
-        <template:content/>
-    </body>
-</html>
--- test/old/security.php
+++
@@ -1,5 +0,0 @@
-<h1>security</h1>
-<?php
-pr('Testing Basic Transport Authentication...');
-assert('$auth = new Authentication("Basic");');
-assert('$auth->setAcl("Array");');
