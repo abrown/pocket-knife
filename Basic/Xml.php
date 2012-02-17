3ae588e10c12c5ffaded4db08be0bda08e67993a
Andrew Brown <andrew@casabrown.com>
2012-02-17 15:32:01 -0500

    Fix encoding/decoding of XML strings

--- Basic/Xml.php
+++ Basic/Xml.php
@@ -11,80 +11,80 @@
  * @uses
  * @example
  */
-class BasicXml{
+class BasicXml {
 
     /**
      * Returns an XML-encoded string
      * @param any $object
      * @return string
      */
-    public static function xml_encode($thing){
+    public static function xml_encode($thing) {
         $w = new XMLWriter;
         $w->openMemory();
         $w->startDocument('1.0', 'UTF-8');
         // get root name
-        if( is_scalar($thing) ){
-            $name = 'element';
-        }
-        elseif( is_object($thing) ){
+        if (is_scalar($thing)) {
+            $name = 'scalar';
+        } elseif (is_object($thing)) {
             $name = 'object';
-        }
-        elseif( is_array($thing) ){
+        } elseif (is_array($thing)) {
             $name = 'array';
-        }
-        else{
-            throw new ExceptionSettings("Cannot encode an object of type ".gettype($object));
+        } else {
+            throw new ExceptionSettings("Cannot encode an object of type " . gettype($object));
         }
         // encode recursively
-        BasicXml::_xml_encode($object, $name, $w);
+        BasicXml::_xml_encode($thing, $name, $w);
         // output
         $w->endDocument();
-        return $w->outputMemory(TRUE);
+        return trim($w->outputMemory(TRUE));
     }
 
     /**
      * Handles recursive encoding of objects
-     * @param mixed $object
+     * @param mixed $thing
      * @param string $name
      * @param XMLWriter $writer
      * @throws ExceptionSettings
      */
-    private static function _xml_encode($object, $name, $writer){
-        if( is_scalar($object) ){
+    private static function _xml_encode($thing, $name, $writer) {
+        if (is_scalar($thing)) {
             $writer->startElement($name);
             $writer->startAttribute('type');
-            $writer->text(gettype($object));
+            $writer->text(gettype($thing));
             $writer->endAttribute();
-            $writer->text($object);
+            $writer->text($thing);
             $writer->endElement();
-        }
-        elseif( is_object($object) ){
+        } elseif (is_object($thing)) {
             $writer->startElement($name);
             $writer->startAttribute('type');
-            $writer->text(get_class($object));
+            $writer->text(get_class($thing));
             $writer->endAttribute();
-            if( get_parent_class($object) ){
+            if (get_parent_class($thing)) {
                 $writer->startAttribute('extends');
-                $writer->text(get_parent_class($object));
+                $writer->text(get_parent_class($thing));
                 $writer->endAttribute();
             }
-            foreach( $object as $key => $value ){
-                BasicXml::_xml_encode($object, $key, $writer);           
+            // encode each property
+            $public_properties = get_public_vars($thing);
+            foreach ($public_properties as $property => $value) {
+                BasicXml::_xml_encode($value, $property, $writer);
             }
             $writer->endElement();
-        }
-        elseif( is_array($object) ){
+        } elseif (is_array($thing)) {
             $writer->startElement($name);
             $writer->startAttribute('type');
             $writer->text('array');
             $writer->endAttribute();
-            foreach( $object as $key => $value ){
-                BasicXml::_xml_encode($object, $key, $writer);           
+            // encode each element
+            foreach ($thing as $key => $value) {
+                if (is_int($key))
+                    $key = 'element';
+                BasicXml::_xml_encode($value, $key, $writer);
             }
             $writer->endElement();
         }
-        else{
-            throw new ExceptionSettings("Cannot encode an object of type ".gettype($object));
+        else {
+            throw new ExceptionForbidden("Cannot encode an object of type " . gettype($thing));
         }
     }
 
@@ -93,47 +93,66 @@ class BasicXml{
      * @param string $string
      * @return stdClass
      */
-    public static function xml_decode($string){
-        $xml = XMLReader::XML($xmlstr);
-        while($xml->read()) {
-            switch($xml->nodeType) {
-                case XMLReader::ELEMENT:
-                    switch($xml->name) {
-                        case 'data': continue;
-                        case 'int': case 'string': case 'bool': case 'double':
-                            return $xml->getAttribute('value');
-                        case 'array':
-                            $in_array = true;
-                            $data = array();
-                            break;
-                        case 'object':
-                            $in_object = true;
-                            $class = $xml->getAttribute('class');
-                            $data = new $class;
-                            break;
-                        case 'entry':
-                            if ($in_array) {
-                                $data[$xml>getAttribute('key')] = $xml>getAttribute('value');
-                            }
-                            break;
-                        case 'property':
-                            if ($in_object) {
-                                $prop = $xml->getAttribute('name');
-                                $data->{$prop} = $xml>getAttribute('value');
-                            }
-                            break;
-                        default: /* What else is there? */
-                            return null;
-                    }
-                    break;
-                case XMLReader::END_ELEMENT:
-                    if ($xml->name == 'data') return $data;
-                    break;
-                default: break;
-            }
-        }
-        return $data;
+    public static function xml_decode($string) {
+        $reader = XMLReader::XML($string);
+        $reader->read();
+        $variable = BasicXml::_xml_decode($reader);
+        return $variable;
     }
 
-
+    /**
+     * Handles the recursive decoding of objects
+     * @param XMLReader $reader
+     * @return mixed
+     */
+    private function _xml_decode($reader) {
+        //pr($reader->name);
+        $variable = null;
+        // get scalar
+        $scalars = array('boolean', 'integer', 'double', 'string', 'null');
+        if ($reader->name == 'scalar' || in_array($reader->getAttribute('type'), $scalars)) {
+            $type = $reader->getAttribute('type');
+            $variable = $reader->readString();
+            settype($variable, $type);
+        }
+        // get array
+        elseif ($reader->name == 'array' || $reader->getAttribute('type') == 'array') {
+            $variable = array();
+            // loop through array elements
+            $depth = $reader->depth;
+            while ($reader->read()) {
+                if( $reader->depth <= $depth ) break;
+                if ($reader->nodeType == XMLReader::ELEMENT) {
+                    if ($reader->name == 'element') {
+                        $variable[] = null;
+                        end($variable);
+                        $cursor = key($variable);
+                    } else {
+                        $variable[$reader->name] = null;
+                        $cursor = $reader->name;
+                    }
+                    // recursively decode the contents
+                    $variable[$cursor] = BasicXml::_xml_decode($reader);
+                }
+            }
+        }
+        // get object
+        else {
+            $type = $reader->getAttribute('type');
+            if (!class_exists($type, false))
+                autoload($type);
+            $variable = new $type;
+            // loop through properties
+            $depth = $reader->depth;
+            while ($reader->read()) {
+                if( $reader->depth <= $depth ) break;
+                if ($reader->nodeType == XMLReader::ELEMENT) {
+                    $property = $reader->name;
+                    $variable->$property = BasicXml::_xml_decode($reader);
+                }
+            }
+        }
+        // return
+        return $variable;
+    }
 }
