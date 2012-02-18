df37b3c5828ac3a7e613fcb6fcd2269140cac6c4
Andrew Brown <andrew@casabrown.com>
2012-02-18 18:15:12 -0500

    Complete RepresentationTest

--- test/RepresentationTest.php
+++ test/RepresentationTest.php
@@ -6,60 +6,65 @@
  */
 
 /**
- * Replaces file_get_contents for classes that use it to get
- * request bodies; set static variable in ServiceTest::$REQUEST_BODY
- * to simulate the request
+ * Override 'get_http_body' to mock HTTP inputs
  */
-namespace Testing;
-function file_get_contents($file){
-    if( $file == 'php://input' ) return ServiceTest::$REQUEST_BODY;
-    else return \file_get_contents($file);
+function get_http_body(){
+    return RepresentationTest::$REQUEST_BODY;
 }
 
-class RepresentationTest extends PHPUnit_Framework_TestCase{
+class RepresentationTest extends PHPUnit_Framework_TestCase {
 
-    public static $REQUEST_BODY = "";
+    public static $REQUEST_BODY;
     
-    public static function setUpBeforeClass(){
+    public static function setUpBeforeClass() {
         // start pocket knife
         $path = dirname(dirname(__FILE__));
-        require $path.'/start.php';
+        require $path . '/start.php';
         // get code
-        autoload('RepresentationInterface');
-        autoload('RepresentationFile');
-        autoload('RepresentationForm');
-        autoload('RepresentationHtml');
-        autoload('RepresentationJson');
-        autoload('RepresentationText');
-        autoload('RepresentationXml');
+        autoload('BasicClass');
+        BasicClass::autoloadAll('Representation');
     }
 
-    public function setUp(){
+    public function setUp() {
         $this->expected = new stdClass();
         $this->expected->a = 1;
-        $this->expected->b = array(2=>'some text');
-        $this->expected->b[3]->c = false;
-        $this->expected->b[3]->d = true;
+        $this->expected->b = new stdClass();
+        $this->expected->b->two = 'some text';
+        $this->expected->b->three->c = false;
+        $this->expected->b->three->d = true;
     }
-    
-    public function testFile(){
+
+    /**
+     * Demonstrates use of a file representation
+     */
+    public function testFile() {
+        $this->markTestSkipped('Requires $_FILES to be populated and file_get_contents() overriden');
+        /**
         self::$REQUEST_BODY = "c29tZSB0ZXh0";
         $expected = "some text";
-        $f = new RepresentationForm();
+        $f = new RepresentationFile();
         $f->receive();
         $actual = $f->getData();
         $this->assertEquals($expected, $actual);
+         */
     }
-    
-    public function testForm(){
-        self::$REQUEST_BODY = "a=1&b[2]=some+text&b[3][c]=false&b[3][d]=true";
+
+    /**
+     * Demonstrates use of a form representation
+     */
+    public function testForm() {
+        $_SERVER['REQUEST_METHOD'] = 'PUT';
+        self::$REQUEST_BODY = "a=1&b[two]=some+text&b[three][c]=false&b[three][d]=true";
         $f = new RepresentationForm();
         $f->receive();
         $actual = $f->getData();
         $this->assertEquals($this->expected, $actual);
     }
-    
-    public function testHtml(){
+
+    /**
+     * Demonstrates use of the HTML representation
+     */
+    public function testHtml() {
         self::$REQUEST_BODY = "sample text";
         $expected = "sample text";
         $f = new RepresentationHtml();
@@ -67,16 +72,22 @@ class RepresentationTest extends PHPUnit_Framework_TestCase{
         $actual = $f->getData();
         $this->assertEquals("sample text", $actual);
     }
-    
-    public function testJson(){
-        self::$REQUEST_BODY = '{"a": 1, b: [null, null, "some text", {"c":false, "d":true}]}';
+
+    /**
+     * Demonstrates use of the JSON representation
+     */
+    public function testJson() {
+        self::$REQUEST_BODY = '{"a": 1, "b": {"two":"some text", "three":{"c":false, "d":true}}}';
         $f = new RepresentationJson();
         $f->receive();
         $actual = $f->getData();
         $this->assertEquals($this->expected, $actual);
     }
-    
-    public function testText(){
+
+    /**
+     * Demonstrates use of the text representation
+     */
+    public function testText() {
         self::$REQUEST_BODY = "sample text";
         $expected = "sample text";
         $f = new RepresentationText();
@@ -85,11 +96,39 @@ class RepresentationTest extends PHPUnit_Framework_TestCase{
         $this->assertEquals($expected, $actual);
     }
     
-    public function testXml(){
-        self::$REQUEST_BODY = '<?xml version="1.0"><root><a>1</a><b><2>some text</2><3><c>false</c><d>true</d></3></b></root>';
+    /**
+     * Demonstrates use of the upload representation
+     */
+    public function testUpload() {
+        // setup
+        $file = tempnam(null, null);
+        file_put_contents($file, 'sample text');
+        $_FILES['test_file'] = array();
+        $_FILES['test_file']['error'] = false;
+        $_FILES['test_file']['name'] = "testfile.txt";
+        $_FILES['test_file']['tmp_name'] = $file;
+        // test upload
+        $expected = 'sample text';
+        $f = new RepresentationUpload();
+        $f->receive();
+        $actual = $f->getData();
+        $this->assertEquals($expected, $actual);
+        // test name
+        $this->assertEquals('testfile.txt', $f->getName());
+        // teardown
+        unlink($file);
+    }
+
+    public function testXml() {
+        self::$REQUEST_BODY = '<?xml version="1.0" ?>'.
+                '<object type="stdClass"><a type="integer">1</a>'.
+                '<b type="stdClass"><two type="string">some text</two>'.
+                '<three type="stdClass"><c type="boolean">false</c>'.
+                '<d type="boolean">true</d></three></b></object>';
         $f = new RepresentationXml();
         $f->receive();
         $actual = $f->getData();
         $this->assertEquals($this->expected, $actual);
     }
+
 }
