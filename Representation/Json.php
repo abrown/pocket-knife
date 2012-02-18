28850a2737a864ea55f5a45c3b7f56f5d40edb5b
Andrew Brown <andrew@casabrown.com>
2012-02-18 16:47:27 -0500

    Fix formatting

--- Representation/Json.php
+++ Representation/Json.php
@@ -9,35 +9,36 @@
  * JSON representation of a RESTful resource.
  * @uses Representation
  */
-class RepresentationJson implements RepresentationInterface{
-    
+class RepresentationJson implements RepresentationInterface {
+
     /**
      * @see Representation::getData()
      */
-    public function getData(){
+    public function getData() {
         return $this->data;
     }
-    
+
     /**
      * @see Representation::setData()
      */
-    public function setData($data){
+    public function setData($data) {
         $this->data = to_object($data);
     }
-    
+
     /**
      * @see Representation::receive()
      */
-    public function receive(){
+    public function receive() {
         $in = file_get_contents('php://input');
         $this->data = json_decode($in);
     }
-    
+
     /**
      * @see Representation::send()
      */
-    public function send(){
-         header('Content-Type: application/json');
+    public function send() {
+        header('Content-Type: application/json');
         echo json_encode($this->data);
     }
+
 }
