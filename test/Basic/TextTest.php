750dbf513c7fef9e84b69054bd8abc237894b46c
Andrew Brown <andrew@casabrown.com>
2012-02-18 16:21:16 -0500

    Add Inflection back in as BasicText

---
+++ test/Basic/TextTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @copyright Copyright 2011 Andrew Brown. All rights reserved.
+ * @license GNU/GPL, see 'help/LICENSE.html'.
+ */
+class BasicTextTest extends PHPUnit_Framework_TestCase {
+
+    public static function setUpBeforeClass() {
+        // start pocket knife
+        $path = dirname(dirname(dirname(__FILE__)));
+        require $path . '/start.php';
+        // get code
+        autoload('BasicClass');
+        BasicClass::autoloadAll('BasicText');
+    }
+
+    /**
+     * Demonstrates usage
+     */
+    public function testUsage() {
+        // paragraph style
+        $expected = 'Blog Posts';
+        $text = new BasicText('blog_posts.php');
+        $actual = $text->toParagraphStyle()->removeFileExtension()->toString();
+        $this->assertEquals($expected, $actual);
+        // camel-case
+        $expected = 'PocketKnifeProject';
+        $text = new BasicText('pocket_knife_project');
+        $actual = $text->toCamelCaseStyle()->toString();
+        $this->assertEquals($expected, $actual);  
+        // pluralize
+        $expected = 'Libraries';
+        $text = new BasicText('Library');
+        $actual = $text->toPlural()->toString();
+        $this->assertEquals($expected, $actual);  
+    }
+
+}
