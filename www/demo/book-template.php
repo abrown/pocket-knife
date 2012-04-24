5f3b8a4e56c713b960d5a10585c61fd7df4ef2a2
andrew <andrew@casabrown.com>
2012-04-24 11:22:36 -0400

    Add HTML templates to demo

--- www/demo/book-template.php
+++ www/demo/book-template.php
@@ -1,2 +1,10 @@
-<?php
-pr($data);
+<!doctype html>
+<html>
+    <head>
+        <link href="<?php echo WebUrl::getDirectoryUrl(); ?>/style.css" media="all" type="text/css" rel="stylesheet" />
+    </head>
+    <body>
+        <header><h1><?php echo $data->getURI(); ?></h1></header>
+        <?php echo WebPage::getResourceTable($data); ?>
+    </body>
+</html>
