b9d9f2ddbb4efecc5081f200d0766cecff05af4d
Andrew Brown <andrew@casabrown.com>
2012-05-27 20:15:27 -0400

    Correct @uses, add documentation

--- Security/Authentication/Header.php
+++ Security/Authentication/Header.php
@@ -7,38 +7,39 @@
 
 /**
  * Provides methods for authenticating users with HTTP headers
+ * @uses SecurityAuthentication, Representation
  */
-class SecurityAuthenticationHeader extends SecurityAuthentication{
+class SecurityAuthenticationHeader extends SecurityAuthentication {
 
     /**
      * The header used to pass the 'username'
      * @var string 
      */
     public $username_header = 'X-PK-USERNAME';
-    
+
     /**
      * The header used to pass the 'password'
      * @var string 
      */
     public $password_header = 'X-PK-PASSWORD';
-    
+
     /**
      * Returns whether the user is logged in; for HTTP authentication, the
      * logs in with every HTTP request
      * @return boolean 
      */
-    public function isLoggedIn(){
+    public function isLoggedIn() {
         return false;
     }
-    
+
     /**
      * Returns the name of the current user
      * @return type 
      */
-    public function getCurrentUser(){
+    public function getCurrentUser() {
         return $_SERVER[$this->username_header];
     }
-    
+
     /**
      * Returns response from a PHP session authentication request
      * @param string $content_type
@@ -63,22 +64,22 @@ class SecurityAuthenticationHeader extends SecurityAuthentication{
         switch ($content_type) {
             case 'application/octet-stream':
             case 'text/plain':
-                $data = 'Submit authentication request as HTTP headers:'."\n";
-                $data .= $this->username_header.': XXX'."\n";
-                $data .= $this->password_header.': XXX'."\n";
+                $data = 'Submit authentication request as HTTP headers:' . "\n";
+                $data .= $this->username_header . ': XXX' . "\n";
+                $data .= $this->password_header . ': XXX' . "\n";
             case 'application/json':
             case 'application/xml':
-                $message = 'Submit authentication request as HTTP headers:'."\n";
-                $message .= $this->username_header.': XXX'."\n";
-                $message .= $this->password_header.': XXX'."\n";
+                $message = 'Submit authentication request as HTTP headers:' . "\n";
+                $message .= $this->username_header . ': XXX' . "\n";
+                $message .= $this->password_header . ': XXX' . "\n";
                 $data = new stdClass();
                 $data->message = $message;
             case 'application/x-www-form-urlencoded':
             case 'multipart/form-data':
             case 'text/html':
-                $data = 'Submit authentication request as HTTP headers:'."<br/>\n";
-                $data .= $this->username_header.': XXX'."<br/>\n";
-                $data .= $this->password_header.': XXX'."<br/>\n";
+                $data = 'Submit authentication request as HTTP headers:' . "<br/>\n";
+                $data .= $this->username_header . ': XXX' . "<br/>\n";
+                $data .= $this->password_header . ': XXX' . "<br/>\n";
                 break;
             default:
                 throw new Error('Unknown content type', 400);
@@ -89,4 +90,5 @@ class SecurityAuthenticationHeader extends SecurityAuthentication{
         $representation->setCode(401);
         $representation->send();
     }
+
 }
