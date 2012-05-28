4c6c65456a77feaa17f62c729cbcc9e57de5a1c9
Andrew Brown <andrew@casabrown.com>
2012-05-27 20:10:07 -0400

    Order dependencies in autoloadAll(); fixes bug where interfaces/parent classes not loaded before children.

--- Basic/Class.php
+++ Basic/Class.php
@@ -9,82 +9,98 @@
  * BasicClass
  * @uses BasicDocumentation, Error
  */
-class BasicClass{
-    
+class BasicClass {
+
     /**
      * Recursively loads all dependent classes
      * @param string $class
      * @return boolean 
      */
-    public static function autoloadAll($class){
+    public static function autoloadAll($class) {
         // check
-        if( class_exists($class, false) ) return true;
+        if (class_exists($class, false))
+            return true;
         // get dependencies
         $dependencies = self::findDependencies($class);
         // get interfaces first
         $interfaces = preg_grep('/interface/i', $dependencies);
-        foreach($interfaces as $_class){
-            if( !interface_exists($_class) ) autoload($_class);
+        foreach ($interfaces as $_class) {
+            if (!interface_exists($_class))
+                autoload($_class);
         }
         // then rest of classes
-        foreach($dependencies as $_class){
-            if( !class_exists($_class) && !interface_exists($_class) ) autoload($_class);
-        }      
+        foreach ($dependencies as $_class) {
+            if (!class_exists($_class) && !interface_exists($_class))
+                autoload($_class);
+        }
         // return
         return true;
     }
-    
+
     /**
      * Performs breadth-first search of dependencies
      * @param string $class
      * @param array $dependencies
      * @return array 
      */
-    public static function findDependencies( $class, $dependencies = array() ){
-        if( !$class ) return array();
+    public static function findDependencies($class, $dependencies = array()) {
         // add self to dependencies
-        if( !in_array($class, $dependencies) ) $dependencies[] = $class;
+        if (!in_array($class, $dependencies))
+            $dependencies[] = $class;
         // get path to class
         $path = self::getPathToClass($class);
         // get dependencies from '@uses' annotation
         $content = file_get_contents($path);
-        if( preg_match('/@uses (.*)/', $content, $matches) ){
+        if (preg_match('/@uses (.*)/', $content, $matches)) {
             $classes = explode(',', $matches[1]);
-            foreach($classes as $_class){
+            foreach ($classes as $_class) {
                 $_class = trim($_class);
-                if( !$_class ) continue;
-                if( in_array($_class, $dependencies) ) continue;
-                else $dependencies[] = $_class;
+                if (!$_class) {
+                    continue;
+                }
+                if (in_array($_class, $dependencies)) {
+                    // remove from current location: needed sooner
+                    $dependencies = array_diff($dependencies, array($_class));
+                    // prepend to front of array
+                    array_unshift($dependencies, $_class);
+                    continue;
+                } else {
+                    // prepend to front of array
+                    array_unshift($dependencies, $_class);
+                }
                 $dependencies = BasicClass::findDependencies($_class, $dependencies);
             }
         }
         return $dependencies;
     }
-    
+
     /**
      * Returns absolute path to the class file
      * @param string $class
      * @return string 
      */
-    public static function getPathToClass($class){
+    public static function getPathToClass($class) {
         $replaced = preg_replace('/([a-z])([A-Z])/', '$1/$2', $class);
         $replaced = str_replace('/', DS, $replaced);
         $path = get_base_dir() . DS . $replaced . '.php';
-        if (!is_file($path)){
-            $message = 'Class '.$class.' not found at: '.$path;
-            if( class_exists('Error') ) throw new Error($message, 500);
-            else trigger_error($message, E_USER_ERROR);
+        if (!is_file($path)) {
+            $message = 'Class ' . $class . ' not found at: ' . $path;
+            if (class_exists('Error'))
+                throw new Error($message, 500);
+            else
+                trigger_error($message, E_USER_ERROR);
         }
         return $path;
     }
-    
+
     /**
      * Returns HTML-formatted documentation for a given class
      * @param string $class
      * @return string HTML
      */
-    public static function getDocumentation($class){
-    	$documentation = new BasicDocumentation($class);
-    	return $documentation->getHtml($class);
+    public static function getDocumentation($class) {
+        $documentation = new BasicDocumentation($class);
+        return $documentation->getHtml($class);
     }
+
 }
