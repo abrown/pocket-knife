723508664872ff7267be8a5342e7e43449abe73d
Andrew Brown <andrew@casabrown.com>
2012-05-27 20:10:35 -0400

    Add correct @uses

--- Resource.php
+++ Resource.php
@@ -16,7 +16,7 @@
  * $output = $resource->HTTP_METHOD($input);
  * // step 3: send
  * $resource->toRepresentation($content_type, $output);
- * @uses Error, ResourceGeneric, ResourceItem, ResourceList
+ * @uses Error
  */
 abstract class Resource {
 
