41aa735ac7da8dc3755c9dd36cc2d90575670780
andrew <andrew@andrew-laptop.(none)>
2012-08-04 17:32:41 -0400

    Added options for finding HTTP content type of request; if apache_request_headers() is not present, use $_SERVER['CONTENT_TYPE'] (for some servers this will imply changes to .htaccess).

--- Web/Http.php
+++ Web/Http.php
@@ -28,7 +28,7 @@ class WebHttp {
      */
     static function getMethod() {
         // check for parameter
-        if( array_key_exists('method', $_GET)){
+        if (array_key_exists('method', $_GET)) {
             return $_GET['method'];
         }
         // else:
@@ -57,21 +57,28 @@ class WebHttp {
         // return
         return $out;
     }
-    
+
     /**
      * Returns the Content-Type of the incoming HTTP request. Checks the request
      * URI first for a 'content-type' parameter; then uses apache-as-a-module
      * to find a content-type; defaults to 'text/html'
      * @return string 
      */
-    static function getContentType(){
-        if( array_key_exists('content-type', $_GET) ){
+    static function getContentType() {
+        if (array_key_exists('content-type', $_GET)) {
             return $_GET['content-type'];
         }
-        $headers = apache_request_headers();
-        if(array_key_exists('Content-Type', $headers)){
-            $parts = explode(';', $headers['Content-Type']);
-            return trim($parts[0]);
+        // try apache_request_headers(); only works when PHP is installed as a module on Apache
+        if (function_exists('apache_request_headers')) {
+            $headers = apache_request_headers();
+            if (array_key_exists('Content-Type', $headers)) {
+                $parts = explode(';', $headers['Content-Type']);
+                return trim($parts[0]);
+            }
+        }
+        // try $_SERVER[CONTENT_TYPE]; may need to set a RewriteRule in .htaccess; see stackoverflow.com/questions/5519802
+        if (array_key_exists('CONTENT_TYPE', $_SERVER) && $_SERVER['CONTENT_TYPE']) {
+            return strtok($_SERVER['CONTENT_TYPE'], ';');
         }
         // else
         return 'text/html';
@@ -81,17 +88,19 @@ class WebHttp {
      * Sends HTTP code to client
      */
     static function setCode($code) {
-        if( headers_sent() ) throw new Error('HTTP headers already sent', 400);
+        if (headers_sent())
+            throw new Error('HTTP headers already sent', 400);
         header('HTTP/1.1 ' . intval($code));
     }
-    
+
     /**
      * Sends HTTP content type to client
      * @param string $type 
      */
-    static function setContentType($type){
-        if( headers_sent() ) throw new Error('HTTP headers already sent', 400);
-        header('Content-Type: '.$type);
+    static function setContentType($type) {
+        if (headers_sent())
+            throw new Error('HTTP headers already sent', 400);
+        header('Content-Type: ' . $type);
     }
 
     /**
@@ -99,7 +108,8 @@ class WebHttp {
      * @param string $url
      */
     static function redirect($url) {
-        if( headers_sent() ) throw new Error('HTTP headers already sent', 400);
+        if (headers_sent())
+            throw new Error('HTTP headers already sent', 400);
         header('Location: ' . $url);
         exit();
     }
@@ -115,7 +125,7 @@ class WebHttp {
      */
     static function request($url, $method = 'GET', $content = '', $content_type = 'text/html', $headers = array()) {
         $method = strtoupper($method);
-        $_headers = array_merge( array('Content-type: '.$content_type), $headers );
+        $_headers = array_merge(array('Content-type: ' . $content_type), $headers);
         $options = array('http' =>
             array(
                 'method' => $method,
@@ -127,17 +137,17 @@ class WebHttp {
         // do request
         $response = file_get_contents($url, false, $context);
         // check errors
-        if( $response === false ){
-            throw new Error('Could not open url: '.$url, 404);
+        if ($response === false) {
+            throw new Error('Could not open url: ' . $url, 404);
         }
         // save response code
-        if( !isset($http_response_header) ){
+        if (!isset($http_response_header)) {
             throw new Error('No HTTP request was made', 400);
         }
         $lines = preg_grep('#HTTP/#i', $http_response_header);
         self::$response_code = 0;
-        foreach($lines as $line){
-            if( preg_match('#HTTP/\d.\d (\d\d\d)#i', $line, $matches) ){
+        foreach ($lines as $line) {
+            if (preg_match('#HTTP/\d.\d (\d\d\d)#i', $line, $matches)) {
                 self::$response_code = intval($matches[1]);
                 break;
             }
@@ -145,12 +155,13 @@ class WebHttp {
         // return 
         return $response;
     }
-    
+
     /**
      * Returns HTTP code from last HTTP request made using WebHttp::request()
      * @return int 
      */
-    static function getRequestCode(){
+    static function getRequestCode() {
         return self::$response_code;
     }
+
 }
