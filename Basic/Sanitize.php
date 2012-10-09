6949c23947ef5f307ef1d55615c0d288859be854
Andrew Brown <andrew@casabrown.com>
2012-10-09 07:59:08 -0400

    Add BasicSanitize
    Initial commit; sanitizes input for safe use

---
+++ Basic/Sanitize.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @copyright Copyright 2011 Andrew Brown. All rights reserved.
+ * @license GNU/GPL, see 'help/LICENSE.html'.
+ */
+
+/**
+ * Sanitize input for safe use.
+ * @uses
+ */
+class BasicSanitize{
+    
+    /**
+     * Remove all but letters, numbers, and limited punctuation (space, hyphen,
+     * underscore, period, and comma).
+     * @param string $string
+     * @return string
+     */
+    public static function toText($string){
+        return preg_replace('#[^a-zA-Z0-9 \-_\.,]#', '', $string);
+    }
+}
