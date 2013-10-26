21690ef1ba9a2d66fd4500bd1e88c57d28a3b4d3
Andrew Brown <andrew@casabrown.com>
2013-10-26 16:33:33 -0400

    Add edit link in demo

--- www/demo/template-view-book.php
+++ www/demo/template-view-book.php
@@ -5,9 +5,12 @@
         <link href="<?php echo WebUrl::getDirectoryUrl(); ?>/style.css" media="all" type="text/css" rel="stylesheet" />
     </head>
     <body>
-        <header><h1><?php echo $resource->getURI(); ?></h1></header>
+        <header><h1><i><?php echo $resource->title; ?></i></h1></header>
         <hr class="title"/>
-        <nav><a href="<?php echo WebUrl::createAnchoredUrl('library'); ?>">Back to Library</a></nav>
-        <?php echo WebPage::getResourceTable($resource); ?>
+        <nav>
+            <a href="<?php $_GET['edit'] = 'true'; echo WebUrl::createAnchoredUrl($resource->getURI(), true); ?>">Edit This Book</a>
+            <a href="<?php echo WebUrl::createAnchoredUrl('library'); ?>">Back to Library</a>
+        </nav>
+<?php echo WebPage::getResourceTable($resource); ?>
     </body>
 </html>
