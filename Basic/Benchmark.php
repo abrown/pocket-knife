d78e79b9520f09b6f5849fef2e0bdbafae4d2bf9
Andrew Brown <andrew@casabrown.com>
2014-01-09 12:38:46 -0500

    Add memory testing
    Can test memory usage before and after some code; also included example
    code in documentation

--- Basic/Benchmark.php
+++ Basic/Benchmark.php
@@ -7,82 +7,113 @@
 
 /**
  * BasicBenchmark
+ * @example 
+ * BasicBenchmark::startMemoryTest();
+ * BasicBenchmark::startTimer();
+ * // code to test...
+ * BasicBenchmark::endTimer();
+ * BasicBenchmark::endMemoryTest();
+ * // display
+ * echo BasicBenchmark::getTimeElapsed().'s and '.BasicBenchmark::getMemoryUsed();
+ * // should display something like '0.02354s and 2.472mb'
  * @uses
  */
-class BasicBenchmark{
-    
+class BasicBenchmark {
+
     /**
      * Timer storage
      * @var int 
      */
-    static $start = 0;
-    static $end = 0;
-    
+    static $startTime = 0;
+    static $endTime = 0;
+
     /**
-     * Starts timer
+     * Start timer
      * @return float the microseconds-time when the timer was started
      */
-    public static function startTimer(){
-        self::$start = microtime(true);
-        return self::$start;
+    public static function startTimer() {
+        self::$startTime = microtime(true);
+        return self::$startTime;
     }
-    
+
     /**
-     * Ends timer
+     * End timer
      * @return float the microseconds-time when the timer was stopped
      */
-    public static function endTimer(){
-        self::$end = microtime(true);
-        return self::$end;
+    public static function endTimer() {
+        self::$endTime = microtime(true);
+        return self::$endTime;
     }
-    
+
     /**
-     * Returns time elapsed
+     * Return the time elapsed in the timer
      * @return float the number of seconds from startTime() to endTime()
      */
-    public static function getTime(){
-        return round(self::$end - self::$start, 5);
+    public static function getTimeElapsed() {
+        return round(self::$endTime - self::$startTime, 5);
     }
-    
+
     /**
-     * Returns peak memory usage
+     * Return peak memory usage in a human-readable format
      * @return string 
      */
-    public static function getPeakMemory($unit = null){
+    public static function getPeakMemoryUsed($unit = null) {
         $size = memory_get_peak_usage(false);
         // choose units
-        $units = array('b','kb','mb','gb','tb','pb');
-        if( $unit ){ 
+        $units = array('b', 'kb', 'mb', 'gb', 'tb', 'pb');
+        if ($unit) {
             $i = array_search($unit, $units);
-        }
-        else{
-            $i = floor( log($size, 1024) );
+        } else {
+            $i = floor(log($size, 1024));
         }
         // format size
-        $mem = $size / pow(1024, $i);
-        $mem = @round( $mem, 2 );
+        $mem = round($size / pow(1024, $i), 3);
         // return
-        return $mem.' '.$units[$i];
+        return $mem . $units[$i];
     }
-    
+
     /**
-     * Returns current memory usage
+     * Return current memory usage
      * @return string
      */
-    public static function getMemory($unit = null){
-        $size = memory_get_usage(false);
+    public static function getMemoryUsed($unit = null) {
+        if (self::$startMemory && self::$endMemory) {
+            $size = self::$endMemory - self::$startMemory;
+        } else {
+            $size = memory_get_usage(false);
+        }
         // choose units
-        $units = array('b','kb','mb','gb','tb','pb');
-        if( $unit ){ 
-            $i = array_search($unit, $units);
-        }
-        else{
-            $i = floor( log($size, 1024) );
-        }
+        $units = array('b', 'kb', 'mb', 'gb', 'tb', 'pb');
+        $i = ($unit) ? array_search($unit, $units) : floor(log($size, 1024));
         // format size
-        $mem = $size / pow(1024, $i);
-        $mem = @round( $mem, 2 );
+        $mem = round($size / pow(1024, $i), 3);
         // return
-        return $mem.' '.$units[$i];
+        return $mem . $units[$i];
     }
+
+    /**
+     * Timer storage
+     * @var int 
+     */
+    static $startMemory = 0;
+    static $endMemory = 0;
+
+    /**
+     * Start a memory test, 
+     * @return int
+     */
+    public static function startMemoryTest() {
+        self::$startMemory = memory_get_usage(false);
+        return self::$startMemory;
+    }
+
+    /**
+     * End a memory test
+     * @return int
+     */
+    public static function endMemoryTest() {
+        self::$endMemory = memory_get_usage(false);
+        return self::$endMemory;
+    }
+
 }
